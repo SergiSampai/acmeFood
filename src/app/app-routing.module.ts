cb1c5de95bc3254b70a9a3738172fc44c9fdcc86
Sergi_Sampai <cs151129@uniwa.gr>
2022-09-22 17:11:18 +0300

    Updates components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
+import { StoreComponent } from './store/store.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  //{ path: 'footer' , component: FooterComponent},
-  //{ path: 'header' , component: HeaderComponent},
+  { path: 'stores' , component: StoreComponent},
   { path: 'home' , component: HomeComponent},
   { path: '**' , component: PagenotfoundComponent}
 
