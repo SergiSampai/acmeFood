0ffe8bdb79881c2f012bd42677b9d221743b3b17
Sergi_Sampai <cs151129@uniwa.gr>
2022-09-30 21:18:18 +0300

    Implements data service getStores from back

---
+++ src/app/service/data.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { ApiResponse } from '../model/apiResponse';
+import { Store } from '../model/store';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http: HttpClient) { }
+
+  baseUrl: String = 'http://localhost:8080';
+
+  getStores(): Observable<ApiResponse<Store>> {
+    return this.http.get<ApiResponse<Store>>(this.baseUrl + '/stores');
+  }
+}
