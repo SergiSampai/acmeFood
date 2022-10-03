c5790e63640cdcbfc142396eda6b36f8d7c52a3c
PanagiotisEfstathiou <pefstathiou@outlook.com>
2022-10-03 13:15:06 +0300

    Changes service provider in store component

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,5 +1,5 @@
-import { StoresService } from './../service/stores.service';
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../service/data.service';
 
 @Component({
   selector: 'app-store',
@@ -10,7 +10,7 @@ export class StoreComponent implements OnInit {
 
   stores:any =[];
 
-  constructor(private service:StoresService) { }
+  constructor(private service:DataService) { }
 
   ngOnInit() {
     this.service.getStores()
