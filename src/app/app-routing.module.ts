24dae8f2a9f23dc96f12599a982b320409258cf9
PanagiotisEfstathiou <pefstathiou@outlook.com>
2022-10-03 13:11:27 +0300

    Adds catalog path to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { StoreComponent } from './store/store.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { HomeComponent } from './home/home.component';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CatalogComponent } from './catalog/catalog.component';
 
 const routes: Routes = [
   { path: 'store' , component: StoreComponent},
   { path: 'home' , component: HomeComponent},
+  {path: 'stores/catalog/:id', component: CatalogComponent},
   { path: '',   redirectTo: 'home', pathMatch: 'full' },
   { path: '**' , component: PagenotfoundComponent}
 
